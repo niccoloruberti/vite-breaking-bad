26ca508785bad87a21b523b0d8e6d11fe516d680
niccoloruberti <nikiruberti@gmail.com>
2023-06-22 16:05:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppPokemonList from './components/AppPokemonList.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppPokemonList
+  }
+}
+</script>
+
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss';
+
+</style>
